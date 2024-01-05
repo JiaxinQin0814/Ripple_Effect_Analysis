5b1ce35084dc2a8be97c6e1877f9de4eb22d508b
JiaxinQin0814 <2020201476@ruc.edu.cn>
2024-01-05 03:40:18 +0000

    1

---
+++ factors_experiments/utils/all_imports.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.append("/home/qjx0814/FastEdit")
+sys.path.append("/home/qjx0814/EasyEdit")
+sys.path.append("/home/qjx0814/Ripple_Effect_Analysis/gradient_experiment")
+import torch
+import random
+from fastedit.utils.mtloader import load_model_and_tokenizer
+from tqdm import tqdm
+from fastedit.utils.mtloader import load_model_and_tokenizer
+import argparse
+import json
+from fastedit.utils.generate import generate_fast
+from fastedit.rome import ROMEHyperParams,apply_rome_to_model
+from fastedit.utils.template import Template
+import os
+from transformers import PreTrainedModel, PreTrainedTokenizer, TextStreamer
+torch.cuda.set_device(4)
+import seaborn as sns
+from torch import nn
+import matplotlib.pyplot as plt
+import numpy as np
+import copy
+from texts import *
+import openai
+openai.api_key = "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"
+from tqdm import tqdm
---
+++ factors_experiments/utils/calculating_probability.py
@@ -0,0 +1,37 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from typing import List, Optional
+import torch
+
+
+def calculate_answer_probability(
+    model: AutoModelForCausalLM,
+    tok: AutoTokenizer,
+    prompt: str,
+    answers: List[str],
+):
+    calculate = []
+    calculate_sum = 0
+    for answer in answers:
+        inp_tok = tok(prompt,padding=False,return_tensors="pt").to(next(model.parameters()).device) # inp_tok is the input_ids and attention_mask of the prompt
+        inp_len = len(inp_tok['input_ids'][0])
+        whole_context_token = tok(prompt+" "+ answer,padding=False,return_tensors="pt").to(next(model.parameters()).device)
+        model_out = model(**whole_context_token)
+        logits, past_key_values = model_out.logits, model_out.past_key_values
+        output_logits = logits[:,inp_len-1:-1,:] # output_logits is the logits of the answer, need to remove 1 position
+        length = output_logits.shape[1]
+        softmax_out = torch.nn.functional.softmax(output_logits,dim=-1)
+        answer_logits = softmax_out[0,torch.arange(whole_context_token['input_ids'][0][inp_len:].shape[0]),whole_context_token['input_ids'][0][inp_len:]]
+        calculate.append(torch.prod(answer_logits))
+        calculate_sum += torch.prod(answer_logits)
+    print("prompt:" + prompt)
+    print("answers:" + answers[0])
+    calculate_sum = torch.log(calculate_sum)
+    print("NLL:"+str((-1)*calculate_sum.item()))
+    
+    return {
+        "prompt": prompt,
+        "answers": answers,
+        "NLL": (-1)*calculate_sum.item(),
+    } # return the log probability of  each answer
+    
+    
